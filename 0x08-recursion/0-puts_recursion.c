4db777f0e652418d99048b79fb0eba4e5c93fc5b
Skyinhaler209 <youssef.aboubaker8@gmail.com>
2023-08-02 08:50:38 -0700

    Task-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * __puts_recursion -> a function to prints string
+ *
+ * @s: string parameter
+ *
+ * return void
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
