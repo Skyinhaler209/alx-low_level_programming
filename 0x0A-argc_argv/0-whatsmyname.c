1f43120b59d634fa41b224dac02ecc853a1f4c9a
Skyinhaler209 <youssef.aboubaker8@gmail.com>
2023-08-04 11:45:25 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point for your program
+* @argc: is considered to be the argument count
+* @argv: is considered to be the argument vector (array)
+*
+* Return: always (0) success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s \n", *argv);
+	return (0);
+}
