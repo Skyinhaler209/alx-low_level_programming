e49c47c90cc792b376164448b170f3ddf3211c11
Skyinhaler209 <youssef.aboubaker8@gmail.com>
2023-08-04 11:55:29 -0700

    Task-1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * @argc: is considered to be the argument count
+ * @argv: is considered to be the argument array
+ *
+ * Return: always (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
